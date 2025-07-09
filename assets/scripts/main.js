8acc065e488d590f39223d1fc716ada300d2456c
Seung Ju <seung-ju@seung-ju.com>
2025-07-09 21:50:45 +0900

    feat: animation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,6 +6,22 @@ window.addEventListener('load', function () {
     const galleryElements = mainElement.querySelectorAll('.gallery');
     const galleryImageElements = mainElement.querySelectorAll('.gallery .image-container');
 
+    galleryElements.forEach(function (element) {
+        const galleryImageElements = element.querySelectorAll('.gallery .image-container');
+        galleryImageElements.forEach(function (element, elementIndex) {
+            const boundingClientRect = element.getBoundingClientRect();
+            const elementWidth = boundingClientRect.width - 75;
+            const visibleRight = Math.min(boundingClientRect.right, windowWidth);
+            const visibleWidth = Math.max(0, visibleRight - boundingClientRect.left) - 75;
+            const visibilityRatio = visibleWidth / elementWidth;
+            setTimeout(function () {
+                element.style.opacity = '' + visibilityRatio;
+                element.style.transform = 'translateY(0%)';
+                element.dataset.ready = 'true';
+            }, elementIndex * 100);
+        });
+    })
+
     const maxScrollWidth = Math.max.apply(
         null,
         Array.from(galleryElements).map(function (element) {
@@ -32,16 +48,23 @@ window.addEventListener('load', function () {
             const scrollPercent = Math.min(scrollY / (scrollHeight - windowHeight), 1)
 
             galleryElements.forEach(function (element) {
-                element.style.transform = 'translateX(-' + scrollY + 'px)';
+                let transform = 'translateX(' + -scrollY + 'px)';
+                if (element.dataset.matrix3d) {
+                    transform += ' matrix3d(' + element.dataset.matrix3d + ')';
+                }
+                element.style.transform = transform;
             });
 
-            galleryImageElements.forEach(function (element, i) {
-                const boundingClientRect = element.getBoundingClientRect();
-                const elementWidth = boundingClientRect.width - 75;
-                const visibleRight = Math.min(boundingClientRect.right, windowWidth);
-                const visibleWidth = Math.max(0, visibleRight - boundingClientRect.left) - 75;
-                const visibilityRatio = visibleWidth / elementWidth;
-                element.style.opacity = '' + visibilityRatio;
+            galleryImageElements.forEach(function (element) {
+                if (element.dataset.ready) {
+                    const boundingClientRect = element.getBoundingClientRect();
+                    const elementWidth = boundingClientRect.width - 75;
+                    const visibleRight = Math.min(boundingClientRect.right, windowWidth);
+                    const visibleWidth = Math.max(0, visibleRight - boundingClientRect.left) - 75;
+                    const visibilityRatio = visibleWidth / elementWidth;
+                    element.style.opacity = '' + visibilityRatio;
+                    element.style.transform = 'translateY(' + (10 - visibilityRatio * 10) + '%)';
+                }
             });
 
             const historyYearLine = historyYearContainerElement.querySelector('.history-year-line')
@@ -68,5 +91,24 @@ window.addEventListener('load', function () {
         const translateX = -clientXPercent * translateMargin + translateMargin / 2 + '%';
         const translateY = -clientYPercent * translateMargin + translateMargin / 2 + '%';
         mainBgImgElement.style.transform = 'scale(' + backgroundScale + ') translateX(' + translateX + ') translateY(' + translateY + ')';
+        galleryElements.forEach(function (element, elementIndex) {
+            element.dataset.matrix3d = '1' +
+                ', 0' +
+                ', 0' +
+                ', ' + (clientXPercent - 0.5) * 0.00005 * (elementIndex % 2 ? 1 : -1) + '' +
+                ', 0' +
+                ', 1' +
+                ', 0' +
+                ', ' + (clientYPercent - 0.5) * 0.0001 * (clientYPercent > 0.5 ? elementIndex % 2 ? -1 : 1 : elementIndex % 2 ? 1 : -1) + '' +
+                ', 0' +
+                ', 0' +
+                ', 1' +
+                ', 0' +
+                ', 0' +
+                ', 0' +
+                ', 0' +
+                ', 1';
+            handleGallery();
+        });
     });
 });
