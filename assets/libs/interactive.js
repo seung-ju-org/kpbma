b2fc1c0cdef229fe0ca2aecd06a3a34b6beb32b3
Seung Ju <seung-ju@seung-ju.com>
2025-07-11 09:11:30 +0900

    feat: details interactive

---
+++ assets/libs/interactive.js
@@ -0,0 +1,52 @@
+window.addEventListener('load', function () {
+    function handleGetIsVisibleElement(element, offset = 0) {
+        const boundingClientRect = element.getBoundingClientRect();
+        return (
+            boundingClientRect.y + boundingClientRect.height - offset <
+            window.innerHeight
+        );
+    }
+
+    function handleElementVisible(element, visible) {
+        if (visible) {
+            element.style.opacity = 1;
+            element.style.transform = "translateY(0)";
+        } else {
+            element.style.opacity = 0;
+            element.style.transform = "translateY(30px)";
+        }
+    }
+
+    function handleElementScrollVisible(element, offset = 0) {
+        handleElementVisible(
+            element,
+            handleGetIsVisibleElement(element, offset),
+        );
+    }
+
+    function handler() {
+        const elements = document.querySelectorAll(".interactive-visible");
+
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            const dataset = element.dataset;
+
+            function handleVisible() {
+                if (dataset.interactiveVisible === "true") {
+                    handleElementVisible(element, true);
+                } else {
+                    handleElementScrollVisible(element, dataset.interactiveOffset);
+                }
+            }
+
+            if (element.dataset.interactiveDelay > 0) {
+                setTimeout(handleVisible, dataset.interactiveDelay);
+            } else {
+                handleVisible();
+            }
+        }
+    }
+
+    handler();
+    window.addEventListener("scroll", handler);
+});
