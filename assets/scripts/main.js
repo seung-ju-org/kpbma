0ece27ed728f4156eddb62823537d58ed66c9af3
Seung Ju <seung-ju@seung-ju.com>
2025-07-11 13:12:35 +0900

    feat: main image change

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,11 +6,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const wrapperElement = document.querySelector('#wrapper')
     const introElement = wrapperElement.querySelector('.intro');
     const introHrElement = introElement.querySelector('hr');
+    const mainBgElement = wrapperElement.querySelector('.main-bg');
+    const mainBgImgElements = mainBgElement.querySelectorAll('img');
     const mainElement = wrapperElement.querySelector('main');
     const galleryElements = mainElement.querySelectorAll('.gallery');
     const galleryImageElements = mainElement.querySelectorAll('.gallery .image-container');
-    const smoothContentElement = document.querySelector('#smooth-content');
 
+    const smoothContentElement = document.querySelector('#smooth-content');
     introHrElement.style.transform = 'scaleX(1)';
     setTimeout(function () {
         const introBgImg = introElement.querySelector('.intro-bg-img');
@@ -18,8 +20,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
         setTimeout(function () {
             const introImg = introElement.querySelector('.intro-img');
             introImg.style.opacity = '1';
-            introImg.style.transform = 'translateY(0)';
 
+            introImg.style.transform = 'translateY(0)';
             const introText = introElement.querySelector('h2');
             introText.style.opacity = '1';
             introText.style.transform = 'translateY(0)';
@@ -33,116 +35,130 @@ document.addEventListener("DOMContentLoaded", (event) => {
                         introElement.style.opacity = '0';
                         introHrElement.style.opacity = '0';
 
+                        const backgroundScale = 1.5;
+                        mainBgElement.style.transform = 'scale(' + backgroundScale + ')';
+                        mainBgElement.style.transformOrigin = 'center center';
+
                         setTimeout(function () {
-                            setTimeout(function () {
-                                introElement.style.display = 'none';
-                            }, 1000);
+                            introElement.style.display = 'none';
+                        }, 1000);
 
+                        galleryElements.forEach(function (element) {
+                            const galleryImageElements = element.querySelectorAll('.gallery .image-container');
+                            galleryImageElements.forEach(function (element, elementIndex) {
+                                const boundingClientRect = element.getBoundingClientRect();
+                                const elementWidth = boundingClientRect.width - 75;
+                                const visibleRight = Math.min(boundingClientRect.right, windowWidth);
+                                const visibleWidth = Math.max(0, visibleRight - boundingClientRect.left) - 75;
+                                const visibilityRatio = visibleWidth / elementWidth;
+                                setTimeout(function () {
+                                    element.style.opacity = '' + visibilityRatio;
+                                    element.style.transform = 'translateY(0%)';
+                                    element.dataset.ready = 'true';
+                                }, elementIndex * 100);
+                            });
+                        })
 
-                            galleryElements.forEach(function (element) {
-                                const galleryImageElements = element.querySelectorAll('.gallery .image-container');
-                                galleryImageElements.forEach(function (element, elementIndex) {
-                                    const boundingClientRect = element.getBoundingClientRect();
-                                    const elementWidth = boundingClientRect.width - 75;
-                                    const visibleRight = Math.min(boundingClientRect.right, windowWidth);
-                                    const visibleWidth = Math.max(0, visibleRight - boundingClientRect.left) - 75;
-                                    const visibilityRatio = visibleWidth / elementWidth;
-                                    setTimeout(function () {
-                                        element.style.opacity = '' + visibilityRatio;
-                                        element.style.transform = 'translateY(0%)';
-                                        element.dataset.ready = 'true';
-                                    }, elementIndex * 100);
-                                });
+                        const maxScrollWidth = Math.max.apply(
+                            null,
+                            Array.from(galleryElements).map(function (element) {
+                                return element.scrollWidth;
                             })
+                        );
 
-                            const maxScrollWidth = Math.max.apply(
-                                null,
-                                Array.from(galleryElements).map(function (element) {
-                                    return element.scrollWidth;
-                                })
-                            );
+                        if (maxScrollWidth > 0) {
+                            const minYear = 1885;
+                            const maxYear = 2024;
+                            const years = new Array(maxYear - minYear + 1).fill(null).map(function (_, index) {
+                                return minYear + index;
+                            })
+                            const scrollElement = document.createElement('div');
 
-                            if (maxScrollWidth > 0) {
-                                const minYear = 1885;
-                                const maxYear = 2024;
-                                const years = new Array(maxYear - minYear + 1).fill(null).map(function (_, index) {
-                                    return minYear + index;
-                                })
-                                const scrollElement = document.createElement('div');
+                            scrollElement.style.height = maxScrollWidth + (windowHeight / 2) + 'px';
+                            smoothContentElement.appendChild(scrollElement);
 
-                                scrollElement.style.height = maxScrollWidth + (windowHeight / 2) + 'px';
-                                smoothContentElement.appendChild(scrollElement);
+                            const historyYearContainerElement = mainElement.querySelector('.history-year-container')
 
-                                const historyYearContainerElement = mainElement.querySelector('.history-year-container')
+                            let x = 0
+                            let progress = 0
 
-                                let x = 0
-                                let progress = 0
-
-                                function handleGallery(event) {
-                                    if (event && event.scrollTrigger) {
-                                        progress = event.scrollTrigger.progress;
-                                        x = maxScrollWidth * progress
-                                    }
-
-                                    galleryElements.forEach(function (element) {
-                                        let elementX = -x;
-                                        if (element.dataset.x) {
-                                            elementX += +element.dataset.x
-                                        }
-                                        let transform = 'translateX(' + elementX + 'px)';
-                                        if (element.dataset.y) {
-                                            transform += ' translateY(' + element.dataset.y + 'px)';
-                                        }
-                                        element.style.transform = transform;
-                                    });
-
-                                    galleryImageElements.forEach(function (element) {
-                                        if (element.dataset.ready) {
-                                            const boundingClientRect = element.getBoundingClientRect();
-                                            const elementWidth = boundingClientRect.width - 75;
-                                            const visibleRight = Math.min(boundingClientRect.right, windowWidth);
-                                            const visibleWidth = Math.max(0, visibleRight - boundingClientRect.left) - 75;
-                                            const visibilityRatio = visibleWidth / elementWidth;
-                                            element.style.opacity = '' + visibilityRatio;
-                                            element.style.transform = 'translateY(' + (10 - visibilityRatio * 10) + '%)';
-                                        }
-                                    });
-
-                                    const historyYearLine = historyYearContainerElement.querySelector('.history-year-line')
-                                    historyYearLine.style.transform = 'scaleX(' + progress * 100 + '%)'
-                                    const historyYear = historyYearContainerElement.querySelector('.btn')
-                                    historyYear.style.transform = 'translateX(' + ((windowWidth - historyYear.clientWidth) * progress) + 'px)'
-                                    historyYear.innerHTML = years.find(function (_, index, array) {
-                                        return (index * (1 / (array.length - 1))) >= progress;
-                                    });
+                            function handleGallery(event) {
+                                if (event && event.scrollTrigger) {
+                                    progress = event.scrollTrigger.progress;
+                                    x = maxScrollWidth * progress
                                 }
 
-                                ScrollSmoother.create({
-                                    smooth: 1.5,
-                                    speed: 0.75,
-                                    effects: true,
-                                    onUpdate: handleGallery
+                                galleryElements.forEach(function (element) {
+                                    let elementX = -x;
+                                    if (element.dataset.x) {
+                                        elementX += +element.dataset.x
+                                    }
+                                    let transform = 'translateX(' + elementX + 'px)';
+                                    if (element.dataset.y) {
+                                        transform += ' translateY(' + element.dataset.y + 'px)';
+                                    }
+                                    element.style.transform = transform;
                                 });
+
+                                galleryImageElements.forEach(function (element) {
+                                    if (element.dataset.ready) {
+                                        const boundingClientRect = element.getBoundingClientRect();
+                                        const elementWidth = boundingClientRect.width - 75;
+                                        const visibleRight = Math.min(boundingClientRect.right, windowWidth);
+                                        const visibleWidth = Math.max(0, visibleRight - boundingClientRect.left) - 75;
+                                        const visibilityRatio = visibleWidth / elementWidth;
+                                        element.style.opacity = '' + visibilityRatio;
+                                        element.style.transform = 'translateY(' + (10 - visibilityRatio * 10) + '%)';
+                                    }
+                                });
+
+                                const historyYearLine = historyYearContainerElement.querySelector('.history-year-line')
+                                historyYearLine.style.transform = 'scaleX(' + progress * 100 + '%)'
+                                const historyYear = historyYearContainerElement.querySelector('.btn')
+                                historyYear.style.transform = 'translateX(' + ((windowWidth - historyYear.clientWidth) * progress) + 'px)'
+                                const year = years.find(function (_, index, array) {
+                                    return (index * (1 / (array.length - 1))) >= progress;
+                                });
+                                historyYear.innerHTML = "" + year;
+
+                                mainBgImgElements.forEach(function (element) {
+                                    element.style.opacity = '0';
+                                });
+
+                                for (let i = mainBgImgElements.length - 1; i >= 0; i -= 1) {
+                                    const element = mainBgImgElements[i];
+
+                                    const elementYear = element.dataset.year;
+
+                                    if (year >= elementYear) {
+                                        element.style.opacity = '0.9';
+                                        break;
+                                    }
+                                }
                             }
 
-                            const mainBgImgElement = wrapperElement.querySelector('.main-bg img');
-                            const backgroundScale = 1.5
-                            mainBgImgElement.style.transform = 'scale(' + backgroundScale + ')';
-                            mainBgImgElement.style.transformOrigin = 'center center';
-                            document.body.addEventListener('mousemove', function (event) {
-                                const clientXPercent = event.clientX / windowWidth;
-                                const clientYPercent = event.clientY / windowHeight;
-                                const translateMargin = 15;
-                                const translateX = -clientXPercent * translateMargin + translateMargin / 1 + '%';
-                                const translateY = -clientYPercent * translateMargin + translateMargin / 1 + '%';
-                                mainBgImgElement.style.transform = 'scale(' + backgroundScale + ') translateX(' + translateX + ') translateY(' + translateY + ')';
-                                galleryElements.forEach(function (element) {
-                                    element.dataset.x = '' + (clientXPercent - 0.5) * 25;
-                                    element.dataset.y = '' + (clientYPercent - 0.5) * 25;
-                                    handleGallery();
-                                });
+                            handleGallery();
+                            ScrollSmoother.create({
+                                smooth: 1.5,
+                                speed: 0.75,
+                                effects: true,
+                                onUpdate: handleGallery
                             });
-                        }, 500);
+                        }
+
+                        document.body.addEventListener('mousemove', function (event) {
+                            const clientXPercent = event.clientX / windowWidth;
+                            const clientYPercent = event.clientY / windowHeight;
+                            const translateMargin = 15;
+                            const translateX = -clientXPercent * translateMargin + translateMargin / 1 + '%';
+                            const translateY = -clientYPercent * translateMargin + translateMargin / 1 + '%';
+                            mainBgElement.style.transform = 'scale(' + backgroundScale + ') translateX(' + translateX + ') translateY(' + translateY + ')';
+                            galleryElements.forEach(function (element) {
+                                element.dataset.x = '' + (clientXPercent - 0.5) * 25;
+                                element.dataset.y = '' + (clientYPercent - 0.5) * 25;
+                                handleGallery();
+                            });
+                        });
                     }, 1000);
                 }, 500);
             }, 1000);
