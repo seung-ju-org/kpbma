a96842704c89bb4d1760f721af02da3ea25ec2de
Seung Ju <seung-ju@seung-ju.com>
2025-07-30 20:08:39 +0900

    fix: highlight end trigger

--- assets/scripts/highlight.js
+++ assets/scripts/highlight.js
@@ -2,12 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const stickyPanelElements = document.querySelectorAll('.sticky-panel');
 
     stickyPanelElements.forEach(function (element) {
-        const stickyAreaElement = element.querySelector('.sticky-area')
+        const stickyAreaElement = element.querySelector('.sticky-area');
+        const stickyAreaBoundingClientRect = stickyAreaElement.getBoundingClientRect();
         ScrollTrigger.create({
             trigger: stickyAreaElement,
             pin: true,
             start: "-=100",
-            end: "+=200",
+            endTrigger: element,
+            end: "bottom +=" + (stickyAreaBoundingClientRect.height + 100)
         });
     })
 });
