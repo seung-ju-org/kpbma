dec36a09547d2b6b02c15b9f9be750690a56d285
Seung Ju <seung-ju@seung-ju.com>
2025-09-18 15:29:53 +0900

    feat: industrial details fixed-area animation

--- assets/scripts/board.js
+++ assets/scripts/board.js
@@ -6,6 +6,7 @@ window.addEventListener('load', function () {
     const boardDetailMetaTitleElement = boardDetailElement.querySelector('.board-detail .meta-container .meta .title');
     const boardDetailMetaContainerElement = boardDetailElement.querySelector('.board-detail .meta-container');
     const boardDetailBreadcrumbItems = boardDetailElement.querySelectorAll('.board-detail .breadcrumb li a');
+    const fixedAreaElement = document.querySelector('.fixed-area');
 
     window.addEventListener('scroll', function () {
         const scrollY = window.scrollY;
@@ -30,6 +31,12 @@ window.addEventListener('load', function () {
             boardDetailBreadcrumbItems.forEach(function (element) {
                 element.style.color = null;
             });
+        }
+
+        if (scrollY >= (window.innerHeight - 80)) {
+            fixedAreaElement.style.top = '80px';
+        } else {
+            fixedAreaElement.style.top = '0px';
         }
     });
 });
